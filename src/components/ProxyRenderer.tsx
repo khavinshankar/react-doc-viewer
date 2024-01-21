88a5f73d135034f1a1723b8b6b085811b5bae231
Khavin Shankar <khavinshankar@gmail.com>
2024-01-21 10:57:07 +0530

    fix rerendering issue

--- src/components/ProxyRenderer.tsx
+++ src/components/ProxyRenderer.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useCallback } from "react";
 import styled, { keyframes } from "styled-components";
 import { setRendererRect } from "../store/actions";
-import { IStyledProps } from "../models";
+import { DocRenderer, IConfig, IDocument, IStyledProps } from "../models";
 import { getFileName } from "../utils/getFileName";
 import { useDocumentLoader } from "../hooks/useDocumentLoader";
 import { useWindowSize } from "../hooks/useWindowSize";
@@ -9,27 +9,40 @@ import { LinkButton } from "./common";
 import { LoadingIcon } from "./icons";
 import { LoadingTimeout } from "./LoadingTimout";
 import { useTranslation } from "../hooks/useTranslation";
+import { IMainState } from "../store/mainStateReducer";
 
-export const ProxyRenderer: FC = () => {
-  const { state, dispatch, CurrentRenderer } = useDocumentLoader();
-  const { documents, documentLoading, currentDocument, config } = state;
-  const size = useWindowSize();
-  const { t } = useTranslation();
+type ContentsProps = {
+  documents: IDocument[];
+  documentLoading: boolean | undefined;
+  config: IConfig | undefined;
+  currentDocument: IDocument | undefined;
+  fileName: string;
+  CurrentRenderer: DocRenderer | null | undefined;
+  state: IMainState;
+  t: (
+    key:
+      | "noRendererMessage"
+      | "documentNavInfo"
+      | "downloadButtonLabel"
+      | "brokenFile"
+      | "msgPluginRecipients"
+      | "msgPluginSender"
+      | "pdfPluginLoading"
+      | "pdfPluginPageNumber",
+    variables?: Record<string, string | number>
+  ) => string;
+};
 
-  const containerRef = useCallback(
-    (node: HTMLDivElement) => {
-      node && dispatch(setRendererRect(node?.getBoundingClientRect()));
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [size],
-  );
-
-  const fileName = getFileName(
+  const Contents: React.FC<ContentsProps> = ({
+    documents,
+    documentLoading,
+    config,
     currentDocument,
-    config?.header?.retainURLParams || false,
-  );
-
-  const Contents = () => {
+    fileName,
+    CurrentRenderer,
+    state,
+    t,
+  }) => {
     if (!documents.length) {
       return <div id="no-documents"></div>;
     } else if (documentLoading) {
@@ -85,24 +98,53 @@ export const ProxyRenderer: FC = () => {
     }
   };
 
-  return (
-    <Container
-      id="proxy-renderer"
-      data-testid="proxy-renderer"
-      ref={containerRef}
-    >
-      <Contents />
-    </Container>
-  );
-};
+  export const ProxyRenderer: FC = () => {
+    const { state, dispatch, CurrentRenderer } = useDocumentLoader();
+    const { documents, documentLoading, currentDocument, config } = state;
+    const size = useWindowSize();
+    const { t } = useTranslation();
 
-const Container = styled.div`
+    const containerRef = useCallback(
+      (node: HTMLDivElement) => {
+        node && dispatch(setRendererRect(node?.getBoundingClientRect()));
+      },
+      [size]
+    );
+
+    const fileName = getFileName(
+      currentDocument,
+      config?.header?.retainURLParams || false
+    );
+
+    return (
+      <Container
+        id="proxy-renderer"
+        data-testid="proxy-renderer"
+        ref={containerRef}
+      >
+        <Contents
+          {...{
+            state,
+            documents,
+            documentLoading,
+            config,
+            currentDocument,
+            fileName,
+            CurrentRenderer,
+            t,
+          }}
+        />
+      </Container>
+    );
+  };
+
+  const Container = styled.div`
   display: flex;
   flex: 1;
   overflow-y: auto;
 `;
 
-const LoadingContainer = styled.div`
+  const LoadingContainer = styled.div`
   display: flex;
   flex: 1;
   height: 75px;
@@ -110,7 +152,7 @@ const LoadingContainer = styled.div`
   justify-content: center;
 `;
 
-const spinAnim = keyframes`
+  const spinAnim = keyframes`
   from {
     transform: rotate(0deg);
   }
@@ -119,14 +161,14 @@ const spinAnim = keyframes`
   }
 `;
 
-const LoadingIconContainer = styled.div`
+  const LoadingIconContainer = styled.div`
   animation-name: ${spinAnim};
   animation-duration: 4s;
   animation-timing-function: linear;
   animation-iteration-count: infinite;
 `;
 
-const DownloadButton = styled(LinkButton)`
+  const DownloadButton = styled(LinkButton)`
   width: 130px;
   height: 30px;
   background-color: ${(props: IStyledProps) => props.theme.primary};
